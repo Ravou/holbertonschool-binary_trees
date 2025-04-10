a7bffce9547172cd5066abd89e09eab7b58c5967
Ravou <oliviaravou@hotmail.fr>
2025-04-10 12:24:55 +0200

    correction

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,7 +15,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left != NULL)
-		left_height = 1 + binary_tree_hieght(tree->left);
+		left_height = 1 + binary_tree_height(tree->left);
 
 	if (tree->right != NULL)
 		right_height = 1 + binary_tree_height(tree->right);
